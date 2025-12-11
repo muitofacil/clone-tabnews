2efcf58b0eb87e61bb923417935da76497fc321f
muitofacil <119463806+muitofacil@users.noreply.github.com>
2025-12-11 11:47:45 +0000

    refazendo a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Isso está ficando legal Ans Lígia</h1>
+    return <h1>Esta é uma mensagem pública na internet que diz uma coisa muito importante: Ana Lígia, eu amo você. Nós temos a melhor família do mundo! Temos que guiá-la da melhor maneira possível. Vamos conseguir!</h1>
 }
 
 export default Home;
